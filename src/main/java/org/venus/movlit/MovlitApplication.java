c68dce2ad16fd94341569d56dad60e9f04531867
Yoonkie96 <zoobon63@gmail.com>
2024-12-23 11:12:18 +0900

    feat #1: Initialization Movlit Project

---
+++ src/main/java/org/venus/movlit/MovlitApplication.java
@@ -0,0 +1,13 @@
+package org.venus.movlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovlitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovlitApplication.class, args);
+	}
+
+}
