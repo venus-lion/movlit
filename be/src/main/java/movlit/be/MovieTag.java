44049e1ec8b0ed1a1b6572ab16f9422adf6ba34d
Yoonkie96 <zoobon63@gmail.com>
2024-12-24 17:01:31 +0900

    feat. Movie Tag 엔티티 추가 생성

---
+++ be/src/main/java/movlit/be/MovieTag.java
@@ -0,0 +1,45 @@
+package movlit.be;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class MovieTag {
+
+    @Id
+    private MovieTagId movieTagId;
+
+    @MapsId("movieId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id", updatable = false, insertable = false)
+    private Movie movie;
+
+    private String name;
+    private LocalDateTime regDt;
+    private LocalDateTime updDt;
+    private boolean delYn;
+
+    @Builder
+    public MovieTag(MovieTagId movieTagId, Movie movie, String name, LocalDateTime regDt, LocalDateTime updDt,
+                    boolean delYn) {
+        this.movieTagId = movieTagId;
+        this.movie = movie;
+        this.name = name;
+        this.regDt = regDt;
+        this.updDt = updDt;
+        this.delYn = delYn;
+    }
+
+}
