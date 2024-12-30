758311726fd0fe160c2aa3b11dd0cf147a5687cb
devbattery <devbattery@outlook.com>
2024-12-30 17:18:32 +0900

    Feat(be) #19: 영화 상세 페이지 크루 조회 API 구현 및 테스트

--- be/src/test/resources/data.sql
+++ be/src/test/resources/data.sql
@@ -1,27 +1,315 @@
-INSERT INTO movie (id, title, original_title, overview, popularity, poster_path, backdrop_path, release_date,
-                   original_language, vote_count, vote_average, production_country, runtime, status, tagline, reg_dt,
-                   upd_dt, del_yn, heart_count)
-VALUES (1, '에픽 어드벤처', 'Epic Adventure', '세상을 구할 영웅의 여정.', 85.5, '/poster1.jpg', '/backdrop1.jpg', '2023-10-26', 'en',
-        500, 7.8, 'US', 120, 'Released', '잊을 수 없는 여정', '2023-11-15 10:00:00', '2023-11-15 10:00:00', false, 1500),
-       (2, '로맨틱 코미디', 'Comédie Romantique', '사랑과 웃음이 넘치는 유쾌한 이야기.', 62.1, '/poster2.jpg', '/backdrop2.jpg',
-        '2023-09-15', 'fr', 300, 6.5, 'FR', 95, 'Released', '사랑이 넘치는', '2023-11-15 10:05:00', '2023-11-15 10:05:00',
-        false, 800),
-       (3, '애니: 더 파이널', 'アニメ：ザ・ファイナル', '사랑받는 애니메이션 시리즈의 장대한 결말.', 92.8, '/poster3.jpg', '/backdrop3.jpg',
-        '2023-11-02', 'ja', 700, 8.2, 'JP', 140, 'Released', '한 시대의 끝', '2023-11-15 10:10:00', '2023-11-15 10:10:00',
-        false, 2000),
-       (4, '강렬한 드라마', 'Drama Intenso', '인간 관계의 복잡성을 탐구하는 드라마.', 45.3, '/poster4.jpg', '/backdrop4.jpg', '2023-08-20',
-        'es', 150, 5.9, 'ES', 110, 'Released', '열정과 배신의 이야기', '2023-11-15 10:15:00', '2023-11-15 10:15:00', false, 300),
-       (5, 'SF 스펙터클', 'Sci-Fi Spectacle', '상상의 경계를 넓히는 시각적 SF 영화.', 78.6, '/poster5.jpg', '/backdrop5.jpg',
-        '2023-12-25', 'en', 450, 7.5, 'US', 135, 'Post Production', '미래는 지금이다', '2023-11-15 10:20:00',
-        '2023-11-15 10:20:00', false, 1200),
-       (6, '스릴 넘치는 액션', '스릴 넘치는 액션', '긴장을 늦출 수 없는 하이옥탄 액션 영화.', 55.9, '/poster6.jpg', '/backdrop6.jpg', '2023-07-10',
-        'ko', 250, 6.2, 'KR', 88, 'Released', '논스톱 액션', '2023-11-15 10:25:00', '2023-11-15 10:25:00', false, 650),
-       (7, '미스터리 언베일드', 'Mystery Unveiled', '마지막까지 추측하게 만드는 서스펜스 미스터리.', 70.2, '/poster7.jpg', '/backdrop7.jpg',
-        '2023-10-12', 'en', 400, 7.1, 'GB', 105, 'Released', '모든 단서가 위험으로 이어진다', '2023-11-15 10:30:00',
-        '2023-11-15 10:30:00', false, 950),
-       (8, '슈퍼히어로 오리진', 'Superhero Origins', '새로운 슈퍼히어로의 기원 이야기.', 88.0, '/poster8.jpg', '/backdrop8.jpg', '2024-01-18',
-        'en', 600, 8.0, 'US', 128, 'Filming', '새로운 전설이 시작된다', '2023-11-15 10:35:00', '2023-11-15 10:35:00', false,
-        1800),
-       (9, '역사적 서사시', 'Historisches Epos', '과거를 생생하게 재현하는 역사적 서사시.', 40.5, '/poster9.jpg', '/backdrop9.jpg',
-        '2023-06-05', 'de', 100, 5.5, 'DE', 92, 'Released', '용기와 희생의 이야기', '2023-11-15 10:40:00', '2023-11-15 10:40:00',
-        false, 250);
+INSERT INTO venus.movie (id, backdrop_path, del_yn, heart_count, original_language, original_title, overview,
+                         popularity, poster_path, production_country, reg_dt, release_date, runtime, status, tagline,
+                         title, upd_dt, vote_average, vote_count)
+VALUES (767, '/ze6Mx4QE5mQVXRYmG2flncqqle4.jpg', false, 0, 'en', 'Harry Potter and the Half-Blood Prince',
+        '어둠의 세력이 더욱 강력해져 머글 세계와 호그와트까지 위협해온다. 위험한 기운을 감지한 덤블도어 교수는 다가올 전투에 대비하기 위해 해리 포터와 함께 대장정의 길을 나선다. 볼드모트를 물리칠 수 있는 유일한 단서이자 그의 영혼을 나누어 놓은 7개의 호크룩스를 파괴하는 미션을 수행해야만 하는 것! 또한 덤블도어 교수는 호크룩스를 찾는 기억여행에 결정적 도움을 줄 슬러그혼 교수를 호그와트로 초청한다. 한편 학교에서는 계속된 수업과 함께 로맨스의 기운도 무르익는다. 해리는 자신도 모르게 지니에게 점점 끌리게 되고, 새로운 여자 친구가 생긴 론에게 헤르미온느는 묘한 질투심을 느끼는데...',
+        165.384, '/yrGlb3HLtjGp39mv4MJ5UuIHcj4.jpg', 'GB', '2024-12-30 14:15:35.289146', '2009-07-15', 153, 'Released',
+        '마지막 전투를 준비하라', '해리 포터와 혼혈왕자', '2024-12-30 14:15:35.289158', 7.688, 19493);
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10980', 'Harry Potter', 'Daniel Radcliffe', 0, '/iPg0J9UzAlPj1fLEJNllpW9IhGe.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10989', 'Ron Weasley', 'Rupert Grint', 1, '/q2KZZ0ltTEl7Sf8volNFV1JDEP4.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10990', 'Hermione Granger', 'Emma Watson', 2, '/tvPPRGzAzdQFhlKzLbMO1EpuTJI.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('388', 'Horace Slughorn', 'Jim Broadbent', 3, '/s7lXYfrsJoGA4vKmyv61SPgABmR.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('5658', 'Albus Dumbledore', 'Michael Gambon', 4, '/3jdWkDKf4IODbG4JKTeaC7AzxZH.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10993', 'Draco Malfoy', 'Tom Felton', 5, '/4fO0PjiBbNFEnYLPe55xUUP7Wgf.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('4566', 'Severus Snape', 'Alan Rickman', 6, '/7tADZs4ILE93oJ5pAh6mKQFEq2m.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10991', 'Ginny Weasley', 'Bonnie Wright', 7, '/7iJeSYYbysCiQgvoRDs2m4jiROu.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234924', 'Lavender Brown', 'Jessie Cave', 8, '/vVfGAQuqWKS768bFw9sxtHAQAiz.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('140367', 'Luna Lovegood', 'Evanna Lynch', 9, '/mebDQC5FnPVYYRxqmhCVVdl8fVM.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234934', 'Cormac McLaggen', 'Freddie Stroma', 10, '/gvdVQk1imvMBqo8rtFfwvt0V3Eh.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1923', 'Rubeus Hagrid', 'Robbie Coltrane', 11, '/jOHs3xvlwRiiG2CLtso5zzmGCXg.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1283', 'Bellatrix Lestrange', 'Helena Bonham Carter', 12, '/hJMbNSPJ2PCahsP3rNEU39C8GWU.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10978', 'Minerva McGonagall', 'Maggie Smith', 13, '/pxJSXL9NELIsawroTYNQOt5JlZz.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('9191', 'Peter Pettigrew', 'Timothy Spall', 14, '/dEhBrDYNVpfWMDx8vpcrp4pXDwl.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('11207', 'Remus Lupin', 'David Thewlis', 15, '/sNuYyT8ocLlQr3TdAW9CoKVbCU8.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('477', 'Molly Weasley', 'Julie Walters', 16, '/bCTkV2OUgzbJdQEoCk3GesE4DXq.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('11180', 'Argus Filch', 'David Bradley', 17, '/znpkwHitb3pcaJbUmgDqG0YenYp.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('11184', 'Filius Flitwick', 'Warwick Davis', 18, '/nGorSpUIQWAEErxq8KdDcIF6Q00.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('9138', 'Poppy Pomfrey', 'Gemma Jones', 19, '/qpvLa2bfCEbGI83RwDOyIThibQ6.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('15737', 'Narcissa Malfoy', 'Helen McCrory', 20, '/3FyO7Z8WigeCQsUpW4B1x3qfmFx.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('3300', 'Nymphadora Tonks', 'Natalia Tena', 21, '/7kN9NpECZoR4NLXb4SlpHOaJx7T.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('20999', 'Arthur Weasley', 'Mark Williams', 22, '/gdlEDhxSmPv3hWHbpTuwCI5Jrtk.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('60348', 'Fenrir Greyback', 'Dave Legeno', 23, '/rqTATsZ15cUVT9TrkeZ6qANwSey.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('89387', 'Waitress', 'Elarica Johnson', 24, '/iiMfkwUSyUki9Q6YbfoVBOGSheq.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10988', 'Lily Potter', 'Geraldine Somerville', 25, '/vyLIquF45MpN6OOPmk8L7gnmKrY.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('140368', 'George Weasley', 'Oliver Phelps', 26, '/kb00uK7sCDB5K6tiH5rav9kzAfl.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('96851', 'Fred Weasley', 'James Phelps', 27, '/dk7kTamtnbgSA40Lg00aE36hljs.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234923', 'Dean Thomas', 'Alfred Enoch', 28, '/eEVvPHmxRBcEGqtoTujncmwNDhR.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1796454', 'Marcus Belby', 'Robert Knox', 29, '/cg8SAor7WYwiIGrOrAJy8E5TA4q.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('568379', 'Twin Girl 1', 'Amber Evans', 30, NULL, 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('568380', 'Twin Girl 2', 'Ruby Evans', 31, NULL, 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234927', 'Blaise Zabini', 'Louis Cordice', 32, '/zVf3FFoV6TVqMLQqvqbLl5leon3.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234928', 'Pansy Parkinson', 'Scarlett Hefner', 33, '/9n7VU5kA1MU78VwyTD9H115B8jA.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('956224', 'Vincent Crabbe', 'Jamie Waylett', 34, '/2Eg3ImAsq5c5tNAyJFHcFC1lejf.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('11212', 'Gregory Goyle', 'Josh Herdman', 35, '/dUWM0gIhQJayVTp30EsoafhLOWt.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('96841', 'Neville Longbottom', 'Matthew Lewis', 36, '/aPQCLK2gxWOallsFoEwjb1p9lWE.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('568374', 'Nigel Wolpert', 'William Melling', 37, '/wcY9Z3KbaMcBR5R62tRVV6XpjX1.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234929', 'Romilda Vane', 'Anna Shaffer', 38, '/exFFZev9XwyyPlJmHDma4fUSeFy.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234922', 'Seamus Finnigan', 'Devon Murray', 39, '/2z6tYeavTWh7muPvfTOKEZGyjjM.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('174398', 'Katie Bell', 'Georgina Leonidas', 40, '/1xjHlxkxxAh7qltWhz6Ko03Nkd8.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234930', 'Leanne', 'Isabella Laughland', 41, '/3vdUpjDwTxZQbaeI8dE0GOu0qzU.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234926', 'Padma Patil', 'Afshan Azad', 42, '/ehjE60ZM0uqotOLJRJfkZTMEr3S.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234925', 'Parvati Patil', 'Shefali Chowdhury', 43, '/tj2UrD1uSdK1P2v0w65Ps5wuACU.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('40646', 'Mrs Cole', 'Amelda Brown', 44, '/sjywxuVI3oU7harX6nX6L3wmoVO.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1114487', 'Tom Riddle (11 Years)', 'Hero Fiennes Tiffin', 45, '/vNdScND38ytcV4tKEdu3cnGZgSe.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('3327916', 'Skinny Kid', 'Jack Pryor', 46, '/7gIR3ApA8Qc1wW9gmvmPJSTStFR.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1713561', 'Waiter', 'Mark Lockyer', 47, '/bUhTKYhDLpW7aLl7C48yDXdEUHd.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('52888', 'Eldred Worple', 'Paul Ritter', 48, '/hYRSjC5vxrIFZ5sx1rM6V4ZEI8G.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('964834', 'Tom Riddle (16 Years)', 'Frank Dillane', 49, '/sXcn4QnEzvPFV9OiKLvPRyVr7RN.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('12838', 'Male Inferi', 'Joerg Stadler', 50, '/g8aeobWRTBnhJZbLaIzQDZAzXAW.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1802952', 'Female Inferi', 'Caroline Wildi', 51, '/kFd7D8Hourlttsaq90ml9eaWDFo.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('202032', 'Amycus Carrow', 'Ralph Ineson', 52, '/sn3ONJw2pJxMHiCqPwvkaiWr5mc.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234918', 'Alecto Carrow', 'Suzie Toase', 53, '/3YaSRY5I37prcsfSGTUh7v9ceJT.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234919', 'Thorfinn Rowle', 'Rod Hunt', 54, '/frfbuGq12b7bgPaHrThtpAHaCMU.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('234933', 'Cho Chang', 'Katie Leung', 55, '/umKxfBnY4SVl4344of8NniU5MQ0.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('2238736', 'Gryffindor Student (uncredited)', 'Nathan Clarke', 56, '/vtfEoPHdIBb4Q5LDBmZyT6iwWfi.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('1457080', 'Slug Club Party Member (uncredited)', 'Olivia Jewson', 57, '/jCjNBnAr5Eg8BYvoVHVxxUZ60bT.jpg',
+        'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('3324854', 'Year 7 Schoolboy (uncredited)', 'Freddie Rose', 58, '/hIM9zZYDYG2OXsNiDE8TeFqu2Ju.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('11355', 'Lucius Malfoy (uncredited)', 'Jason Isaacs', 59, '/s6XRFjqUsrDJfDQuXPOoExAYPmb.jpg', 'CAST');
+
+INSERT INTO movie_crew (id, char_name, name, order_no, profile_img_url, role)
+VALUES ('10967', NULL, 'Steve Kloves', 0, '/cqXWKsUKcAClzlwzMuShLWVBQPz.jpg', 'DIRECTOR');
+
+
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10980');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10989');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10990');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '388');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '5658');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10993');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '4566');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10991');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234924');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '140367');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234934');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1923');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1283');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10978');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '9191');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '11207');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '477');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '11180');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '11184');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '9138');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '15737');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '3300');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '20999');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '60348');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '89387');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10988');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '140368');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '96851');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234923');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1796454');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '568379');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '568380');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234927');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234928');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '956224');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '11212');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '96841');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '568374');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234929');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234922');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '174398');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234930');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234926');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234925');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '40646');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1114487');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '3327916');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1713561');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '52888');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '964834');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '12838');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1802952');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '202032');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234918');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234919');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '234933');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '2238736');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '1457080');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '3324854');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '11355');
+INSERT INTO venus.movie_r_crew (movie_id, movie_crew_id)
+VALUES (767, '10967');
