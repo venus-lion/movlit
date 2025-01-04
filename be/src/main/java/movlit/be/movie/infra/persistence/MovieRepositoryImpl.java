da85febeac41cf830c281bffd8d3bcde1cbedd00
devbattery <devbattery@outlook.com>
2025-01-04 19:50:39 +0900

    Fix(be) #57: Merge 후 충돌 문제 해결

--- be/src/main/java/movlit/be/movie/infra/persistence/MovieRepositoryImpl.java
+++ be/src/main/java/movlit/be/movie/infra/persistence/MovieRepositoryImpl.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-@Slf4j
 public class MovieRepositoryImpl implements MovieRepository {
 
     private final MovieJpaRepository movieJpaRepository;
