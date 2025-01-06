1d604b5bc6c53796f4250a60aa8b66b0a97fea79
minyoongi96 <zoobon63@gmail.com>
2025-01-07 03:51:02 +0900

    Fix(be) #68: 유저의 찜한 영화와 유사한 장르그룹의 영화 추천 API 완료

--- be/src/main/java/movlit/be/movie/infra/persistence/MovieSearchRepositoryImpl.java
+++ be/src/main/java/movlit/be/movie/infra/persistence/MovieSearchRepositoryImpl.java
@@ -87,8 +87,8 @@ public class MovieSearchRepositoryImpl implements MovieSearchRepository {
 
     @Override
     public List<Movie> searchByUserHeartMovie(Movie movie, Pageable pageable) {
-
-
+        // TODO : 위와 같이 비슷한 로직(가중치 + 1) + [title이 서로 유사 (가중치 +2), 찜한 title과 overview가 유사(가중치 +1.5)]
+        // 최근 찜한 영화 최대 3개 가져와서 장르 HashSet()
         return null;
     }
 
