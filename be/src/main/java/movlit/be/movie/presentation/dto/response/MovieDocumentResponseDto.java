43ea3f51ab9296423e0809ca20495549ed0229f8
Yoonkie96 <zoobon63@gmail.com>
2025-01-08 17:54:01 +0900

    Fix(be) #95. 영화 검색 api

--- be/src/main/java/movlit/be/movie/presentation/dto/response/MovieDocumentResponseDto.java
+++ be/src/main/java/movlit/be/movie/presentation/dto/response/MovieDocumentResponseDto.java
@@ -3,5 +3,5 @@ package movlit.be.movie.presentation.dto.response;
 import java.util.List;
 import movlit.be.movie.domain.document.MovieDocument;
 
-public record MovieDocumentResponseDto(List<MovieDocument> movieList) {
+public record MovieDocumentResponseDto(List<MovieDocument> movieList, long totalPages) {
 }
