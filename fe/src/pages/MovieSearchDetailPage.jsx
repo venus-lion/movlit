c0e36f08c706c3ffe3d16104b76054772793a51c
minyoongi96 <zoobon63@gmail.com>
2025-01-09 23:41:02 +0900

    Fix(fe) 검색 상세 수정

--- fe/src/pages/MovieSearchDetailPage.jsx
+++ fe/src/pages/MovieSearchDetailPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {Link, useParams} from 'react-router-dom';
+import {FaStar, FaRegStar, FaStarHalfAlt} from 'react-icons/fa';
 import './SearchDetailPage.css'; // SearchPage.css 기반
 import axios from 'axios';
 import axiosInstance from "../axiosInstance.js";
@@ -12,6 +13,25 @@ function MovieSearchPage() {
     const [totalPages, setTotalPages] = useState(0);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    // 별을 표시하는 함수
+    const renderStars = (rating) => {
+        // rating 값을 0 ~ 10으로 받을 경우
+        const validRating = Math.max(0, Math.min(10, rating || 0));  // 0 ~ 10 사이로 제한
+
+        // 2점마다 1개의 꽉 찬 별로 환산
+        const fullStars = Math.floor(validRating / 2);  // 꽉 찬 별 개수
+        const halfStar = validRating % 2 >= 1 ? 1 : 0;  // 반쪽 별 여부 (나머지가 1 이상이면 반쪽 별)
+        const emptyStars = 5 - fullStars - halfStar;  // 빈 별 개수 (총 5개 별이므로 나머지)
+
+        return (
+            <>
+                {[...Array(fullStars)].map((_, index) => <FaStar key={`full-${index}`} className="star-icon"/>)}
+                {halfStar === 1 && <FaStarHalfAlt className="star-icon"/>}
+                {[...Array(emptyStars)].map((_, index) => <FaRegStar key={`empty-${index}`} className="star-icon"/>)}
+            </>
+        );
+    };
 
     useEffect(() => {
         console.log("input Str : "+ inputStr);
@@ -57,7 +77,17 @@ function MovieSearchPage() {
                 {movieList.map((movie) => (
                     <div key={movie.movieId} className="search-item-detail">
                         <Link to={`/movie/${movie.movieId}`}>
-                            <img src={movie.posterPath} alt={movie.title}/>
+                            <div className="movie-info">
+                                <img src={movie.posterPath} alt={movie.title}/>
+                                <div className="movie-info">
+                                    <h3 className="movie-title">{movie.title}</h3>
+                                    {renderStars(parseFloat(movie.voteAverage))}
+                                    <span>({Math.round(parseFloat(movie.voteAverage) * 10) / 10})</span>
+                                    <p className="movie-genres">
+                                        {movie.movieGenre.map((g) => g.genreName).join(', ')}
+                                    </p>
+                                </div>
+                            </div>
                             <p>{movie.title}</p>
                         </Link>
                     </div>
