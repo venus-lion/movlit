4c056befdfdf039cce1cefd18a28c1148f9bd7f6
devbattery <devbattery@outlook.com>
2025-01-09 14:03:18 +0900

    [fe] Fix: 프록시 지우기

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -4,20 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080', // Spring Boot 백엔드 주소
-                changeOrigin: true,
-                secure: false, // HTTPS가 아닌 경우 필요
-                // rewrite: (path) => path.replace(/^\/api/, ''), // 필요에 따라 경로 재작성
-            },
-            '/oauth2': {
-                target: 'http://localhost:8080', // Spring Boot 백엔드 주소
-                changeOrigin: true,
-                secure: false, // HTTPS가 아닌 경우 필요
-                // rewrite: (path) => path.replace(/^\/oauth2/, ''), // 필요에 따라 경로 재작성
-            },
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://localhost:8080', // Spring Boot 백엔드 주소
+    //             changeOrigin: true,
+    //             secure: false, // HTTPS가 아닌 경우 필요
+    //             // rewrite: (path) => path.replace(/^\/api/, ''), // 필요에 따라 경로 재작성
+    //         },
+    //         '/oauth2': {
+    //             target: 'http://localhost:8080', // Spring Boot 백엔드 주소
+    //             changeOrigin: true,
+    //             secure: false, // HTTPS가 아닌 경우 필요
+    //             // rewrite: (path) => path.replace(/^\/oauth2/, ''), // 필요에 따라 경로 재작성
+    //         },
+    //     },
+    // },
 });
