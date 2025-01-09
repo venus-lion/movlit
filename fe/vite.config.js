43c3712910c3319ecf8602ab0efe67d43cdd05db
G1Huh <hgw001468@naver.com>
2025-01-09 21:22:08 +0900

    fix[fe] : vite.config 백엔드 연동

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -5,22 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port : 3000
-    }
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:8080', // Spring Boot 백엔드 주소
-    //             changeOrigin: true,
-    //             secure: false, // HTTPS가 아닌 경우 필요
-    //             // rewrite: (path) => path.replace(/^\/api/, ''), // 필요에 따라 경로 재작성
-    //         },
-    //         '/oauth2': {
-    //             target: 'http://localhost:8080', // Spring Boot 백엔드 주소
-    //             changeOrigin: true,
-    //             secure: false, // HTTPS가 아닌 경우 필요
-    //             // rewrite: (path) => path.replace(/^\/oauth2/, ''), // 필요에 따라 경로 재작성
-    //         },
-    //     },
-    // },
+        port : 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // Spring Boot 백엔드 주소
+                changeOrigin: true,
+                secure: false, // HTTPS가 아닌 경우 필요
+                // rewrite: (path) => path.replace(/^\/api/, ''), // 필요에 따라 경로 재작성
+            },
+            '/oauth2': {
+                target: 'http://localhost:8080', // Spring Boot 백엔드 주소
+                changeOrigin: true,
+                secure: false, // HTTPS가 아닌 경우 필요
+                // rewrite: (path) => path.replace(/^\/oauth2/, ''), // 필요에 따라 경로 재작성
+            },
+        },
+    },
 });
