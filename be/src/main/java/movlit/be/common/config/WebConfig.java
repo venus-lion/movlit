13addc69af91ea24e6b1157117b1a7d36c490fef
devbattery <devbattery@outlook.com>
2025-01-09 22:55:01 +0900

    [be] Revert: allowedOrigins * 되돌리기

--- be/src/main/java/movlit/be/common/config/WebConfig.java
+++ be/src/main/java/movlit/be/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // movlit.store 에서 오는 요청 허용
+                .allowedOrigins("https://movlit.store") // movlit.store 에서 오는 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 허용
