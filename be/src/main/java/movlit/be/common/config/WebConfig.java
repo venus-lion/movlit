a7064119193a2af2fa7245abf33ce60bb5d71b3b
devbattery <devbattery@outlook.com>
2025-01-10 13:46:08 +0900

    Feat[be]: yml config 추가

--- be/src/main/java/movlit/be/common/config/WebConfig.java
+++ be/src/main/java/movlit/be/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package movlit.be.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${share.url}")
+    private String url;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://movlit.store") // movlit.store 에서 오는 요청 허용
+                .allowedOrigins(url) // movlit.store 에서 오는 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 허용
