69fa193427c606b334b374ee3f9acb8fdc8b2342
G1Huh <hgw001468@naver.com>
2025-01-20 16:02:09 +0900

    refactor[be] : 그룹채팅 repository 리팩토링

--- be/src/main/java/movlit/be/pub_sub/chatRoom/infra/persistence/GroupChatRepositoryImpl.java
+++ be/src/main/java/movlit/be/pub_sub/chatRoom/infra/persistence/GroupChatRepositoryImpl.java
@@ -1,10 +1,14 @@
 package movlit.be.pub_sub.chatRoom.infra.persistence;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import movlit.be.common.exception.ChatroomNotFoundException;
+import movlit.be.common.util.ids.MemberId;
 import movlit.be.pub_sub.chatRoom.domain.GroupChatroom;
 import movlit.be.pub_sub.chatRoom.domain.repository.GroupChatRepository;
 import movlit.be.pub_sub.chatRoom.infra.persistence.jpa.GroupChatroomJpaRepository;
 import movlit.be.pub_sub.chatRoom.presentation.dto.GroupChatroomResponse;
+import movlit.be.pub_sub.chatRoom.presentation.dto.GroupChatroomResponseDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,6 +21,14 @@ public class GroupChatRepositoryImpl implements GroupChatRepository {
     public GroupChatroomResponse create(GroupChatroom groupChatroom) {
         GroupChatroom savedEntity = groupChatroomJpaRepository.save(groupChatroom);
         return GroupChatroomResponse.of(savedEntity.getGroupChatroomId());
+    }
+
+    @Override
+    public List<GroupChatroomResponseDto> findAllByMemberId(MemberId memberId) {
+        List<GroupChatroomResponseDto> myGroupChatList = groupChatroomJpaRepository.findAllByMemberId(memberId)
+                .orElseThrow(ChatroomNotFoundException::new);
+
+        return myGroupChatList;
     }
 
 }
--- be/src/main/java/movlit/be/pub_sub/chatRoom/presentation/dto/GroupChatroomResponseDto.java
+++ be/src/main/java/movlit/be/pub_sub/chatRoom/presentation/dto/GroupChatroomResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import movlit.be.common.util.ids.GroupChatroomId;
 
 @Getter
@@ -13,6 +14,7 @@ import movlit.be.common.util.ids.GroupChatroomId;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class GroupChatroomResponseDto {
 
     private GroupChatroomId groupChatroomId;
