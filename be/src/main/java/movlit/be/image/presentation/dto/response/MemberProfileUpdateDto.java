18f2a9a00d9b3e5b17009cc38f7d1be1995fcf07
minjikimkim2222 <olivia0929@naver.com>
2025-01-23 16:30:53 +0900

    fix(be) : 멤버 프로필 수정 시, 장르랑 member_r_chatroom 날라가는 에러 해결

---
+++ be/src/main/java/movlit/be/image/presentation/dto/response/MemberProfileUpdateDto.java
@@ -0,0 +1,11 @@
+package movlit.be.image.presentation.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberProfileUpdateDto {
+    private String profileImgUrl;
+
+}
